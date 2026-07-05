eb9c974433ce0dec3a7a59e647ac87c6cb6b881f
enzin00-eng <fabio.enzoo00@gmail.com>
2026-07-05 10:16:26 -0300

    feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -87,6 +87,16 @@ const Home = async () => {
           ))}
         </div>
       </div>
+
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2023 Copyright <span className="font-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
