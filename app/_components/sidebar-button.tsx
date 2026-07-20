f81fe001fe5e49e7a292d8f05d3d29a02453db1c
enzin00-eng <fabio.enzoo00@gmail.com>
2026-07-20 11:23:09 -0300

    chore: add next auth setup

--- app/_components/sidebar-button.tsx
+++ app/_components/sidebar-button.tsx
@@ -1,32 +1,68 @@
 "use client"
 
-import { Calendar1Icon, HomeIcon, LogOutIcon } from "lucide-react"
+import { Calendar1Icon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { quickSearchOptions } from "../_constrants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 
 const SidebarSheet = () => {
   return (
-    <SheetContent>
+    <SheetContent className="gap-0">
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid p-4">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid p-4">
+        <h2 className="text-lg font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger
+            render={
+              <Button size="icon">
+                <LogInIcon />
+              </Button>
+            }
+          ></DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                alt="Login com Google"
+                src="/google.svg"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/*<Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
 
         <div>
           <p className="font-bold">Fábio Enzo</p>
           <p className="text-xs">fabio@email.io</p>
-        </div>
+        </div>*/}
       </div>
 
-      <div className="flex flex-col gap-2 border-b border-solid p-4">
+      <div className="flex flex-col gap-3 border-b border-solid p-4">
         <SheetClose
           render={
             <Button
@@ -48,7 +84,7 @@ const SidebarSheet = () => {
         </Button>
       </div>
 
-      <div className="flex flex-col gap-2 border-b border-solid p-4">
+      <div className="flex flex-col gap-3 border-b border-solid p-4">
         {quickSearchOptions.map((option) => (
           <Button
             key={option.title}
