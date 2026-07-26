e2ad22a4797f67396c7afc6cfbd8dcdcc0278bc0
enzin00-eng <fabio.enzoo00@gmail.com>
2026-07-26 18:54:02 -0300

    refactor: ensure decimal warning is not shown on console

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,10 @@ const Home = async () => {
         {/* AGENDAMENTO */}
         <div className="flex gap-3 overflow-x-auto">
           {confirmedBookings.map((booking) => (
-            <BookingItem key={booking.id} booking={booking} />
+            <BookingItem
+              key={booking.id}
+              booking={JSON.parse(JSON.stringify(booking))}
+            />
           ))}
         </div>
 
